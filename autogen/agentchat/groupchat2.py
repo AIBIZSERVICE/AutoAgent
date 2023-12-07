0ce31affa892bc65ba3577614f646ff858e747a5
Adam Fourney <adamfo@microsoft.com>
2023-12-07 10:27:19 -0800

    Make the moderator roles stronger still!

--- autogen/agentchat/groupchat2.py
+++ autogen/agentchat/groupchat2.py
@@ -95,7 +95,7 @@ class GroupChat2:
 
     def select_speaker_msg(self, agents: List[Agent]):
         """Return the system message for selecting the next speaker. This is always the *first* message in the context."""
-        return f"""You are moderating a conversation between {len(self.agents)} participants who are working together to answer questions and perform tasks. Your role is as a moderator. DON'T DIRECTLY ANSWER THE QUESTIONS OR TASKS, but instead direct the participants to do so. In attendance are the following participants:
+        return f"""You are moderating a conversation between {len(self.agents)} participants who are working together to answer questions and perform tasks. Your role is as a moderator. DON'T DIRECTLY ANSWER THE QUESTIONS OR PERFORM ANY OF THE WORK YOURSELF. IN PARTICULAR, DO NOT WRITE ANY CODE YOURSELF. INSTEAD, DIRECT THE PARTICIPANTS TO DO SO, AS APPROPRIATE. In attendance are the following participants:
 
 {self._participant_roles(agents)}
 
@@ -122,7 +122,7 @@ DO NOT OUTPUT ANYTHING OTHER THAN THIS JSON OBJECT. YOUR OUTPUT MUST BE PARSABLE
         if excluded_agent is not None:
             exclude_speaker_msg = f"\nNote: Don't ask {excluded_agent.name} again, since they just spoke. Instead ask {' or '.join([agent.name for agent in agents])}."
 
-        return f"""Read the above conversation, then carefully decide the following, with a focus on making progress on the task:
+        return f"""Remember, YOUR role is to serve as a moderator. DON'T ANSWER QUESTIONS, CODE, OR PERFORM OTHER WORK YOURSELF. Instead, read the above conversation, then carefully decide the following, with a focus on making progress on the task:
 
     - WHO should speak next? (A valid participant name, selected from this list: {[agent.name for agent in agents]})
     - WHAT should you ask of them? (phrased the way you would actually ask them in conversation)
@@ -135,6 +135,8 @@ Your output should be a perfect JSON object as per below:
         "who": participant_name,
         "what": your_question_or_request
     }}
+
+
 DO NOT OUTPUT ANYTHING OTHER THAN THIS JSON OBJECT. YOUR OUTPUT MUST BE PARSABLE AS JSON.
 """
 
